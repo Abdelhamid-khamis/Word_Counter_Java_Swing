3d55bd114cf9c70eba56602d9f153a462c701836
Hameed Khamis <53490154+Abdelhamid-khamis@users.noreply.github.com>
2024-04-17 21:45:14 +0200

    added more lines to the project

--- src/main/java/program.java
+++ src/main/java/program.java
@@ -1,5 +1,6 @@
 // Java program using label (swing)
 // to display the message “GFG Website Click”
+import java.awt.*;
 import java.io.*;
 import javax.swing.*;
 
@@ -9,25 +10,31 @@ class GFG {
     // Main driver method
     public static void main(String[] args)
     {
-        // Creating instance of JFrame
-        JFrame frame = new JFrame();
+        /*
+        * Create frame object from JFrame class
+            Create 3 button objects, from JButton class
+                Setting different bounds, 2 are squared and 1 is rectangle
+            Adding 3 buttons to the frame
+                Setting size, layout, visibility to the frame
+        * */
 
-        // Creating instance of JButton
-        JButton button = new JButton(" GFG WebSite Click");
+        JFrame frame = new JFrame("Word Counter");
 
-        // x-axis, y-axis, width, height
-        button.setBounds(150, 200, 220, 50);
+        JButton button1 = new JButton("Click Here!");
+        JButton button2 = new JButton("Click Here!");
+        JButton button3 = new JButton("Click Here!");
 
-        // adding button in JFrame
-        frame.add(button);
+        button1.setBounds(150,150,200,200);
+        button2.setBounds(150,150,300,200);
+        button3.setBounds(150,150,500,200);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
 
-        // Width 400 and 500 height
-        frame.setSize(500, 600);
-
-        // using no layout managers
+        frame.setBounds(700,500, 500,500);
+        frame.setVisible(true);
         frame.setLayout(null);
 
-        // making the frame visible
-        frame.setVisible(true);
-    }
 }
+}
+
